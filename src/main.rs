3c2f7b81702a8d8fefebf92568be395623f944f5
huang12zheng <805104533@qq.com>
2022-07-12 00:44:25 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use insta::assert_debug_snapshot;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn test_snapshots() {
+    // let value = vec![1, 2, 3];
+    // assert_debug_snapshot!(value);
+    insta::assert_snapshot!("🫗", "🫗");
+}
